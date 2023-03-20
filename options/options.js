feea8340e526543eea6dfbb7caa7ca415d7f9a7f
Crowdbotics <team@crowdbotics.com>
2023-03-20 15:36:56 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "pckg-39463",
+  url: "https://pckg-39463.botics.co",
+  api: "https://pckg-39463.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
